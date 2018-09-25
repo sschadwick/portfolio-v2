da12e6658228e7f6ad965fc2f97228c62f5799f2
msweeneydev <matthewsweeney@mailup.net>
2018-09-25 22:25:31 +0100

    content change

--- src/content/Tools/index.js
+++ src/content/Tools/index.js
@@ -2,12 +2,9 @@ import React from 'react'
 
 const ToolsContent = () => (
   <>
-    <p>Front-End Developer based in Glasgow, currently working for Ajenta.</p>
-    <p>Current student with The Open University studying Computer Science.</p>
-    <p>
-      Admirer of useful abstractions, monochrome colour schemes and strong
-      typing.
-    </p>
+    <p>My tool of choice is JavaScript and its many flavours.</p>
+    <p>I spend most of my time developing using React, Redux and TypeScript.</p>
+    <p>I'm currently getting to grips with GraphQL and Apollo.</p>
   </>
 )
 
