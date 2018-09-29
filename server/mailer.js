47ed380cadbdf2c49ab6c00f86c545fce2100fb6
msweeneydev <matthewsweeney@mailup.net>
2018-09-29 10:10:41 +0100

    basic form validation added

--- server/mailer.js
+++ server/mailer.js
@@ -16,13 +16,12 @@ exports.handler = function(event, context, callback) {
 
   transporter.verify(function(error, success) {
     if (error) {
-      console.log('setup error', error)
+      console.log('Setup error!', error)
     } else {
-      console.log('Server is ready to take our messages')
+      console.log('Server is setup!')
     }
   })
-  console.log(event)
-  console.log(event.body)
+
   const requestBody = JSON.parse(event.body)
   const { name, email, message } = requestBody
 
