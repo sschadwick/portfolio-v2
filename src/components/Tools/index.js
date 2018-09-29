69d7f8e4b1b23b00f049e294bee256f937c5fbb4
msweeneydev <matthewsweeney@mailup.net>
2018-09-29 19:49:26 +0100

    header/content changes

--- src/components/Tools/index.js
+++ src/components/Tools/index.js
@@ -1,8 +1,48 @@
 import React from 'react'
+import './tools.scss'
 
 const Tools = () => (
-  <div className="section">
-    <p>test</p>
+  <div className="tools">
+    <div class="languages__list list">
+      <h4>languages</h4>
+      <ul>
+        <li>HTML5</li>
+        <li>CSS3 / SASS</li>
+        <li>JavaScript (ES6)</li>
+        <li>TypeScript</li>
+        <li>Python / Java</li>
+      </ul>
+    </div>
+    <div class="libraries__list list">
+      <h4>libraries</h4>
+      <ul>
+        <li>React / Redux</li>
+        <li>jQuery</li>
+        <li>animate.css</li>
+        <li>Bootstrap 4</li>
+        <li>Semantic UI</li>
+      </ul>
+    </div>
+    <div class="concepts__list list">
+      <h4>concepts</h4>
+      <ul>
+        <li>Responsive Design</li>
+        <li>RESTful API</li>
+        <li>Version Control</li>
+        <li>Git Flow</li>
+        <li>Agile / Scrum</li>
+      </ul>
+    </div>
+    <div class="tools__list list">
+      <h4>tools</h4>
+      <ul>
+        <li>Git / GitHub</li>
+        <li>NPM / Yarn</li>
+        <li>Bash / ZSH</li>
+        <li>Gatsby</li>
+        <li>Netlify</li>
+      </ul>
+    </div>
   </div>
 )
 
--- src/content/About/index.js
+++ src/content/About/index.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 const AboutContent = () => (
   <>
-    <p>Front-End Developer based in Glasgow, currently working for Ajenta.</p>
+    <p>
+      Front-End Developer based in Glasgow, currently working for{' '}
+      <a
+        className="link"
+        href="https://byteconf.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Ajenta
+      </a>
+      .
+    </p>
     <p>Current student with The Open University studying Computer Science.</p>
     <p>
       Admirer of useful abstractions, monochrome colour schemes and strong
